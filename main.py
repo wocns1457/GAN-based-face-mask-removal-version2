703a33722e95f458c405d173aaaeb2b4acce50e1
wocns1457 <wocns1457@gmail.com>
2022-02-27 19:37:19 +0900

    Added main.py

---
+++ main.py
@@ -0,0 +1,52 @@
+import argparse
+import os
+import tensorflow as tf
+from datasets import Dataset
+from models import Mask_G, Face_G, Face_D_whole, Face_D_region, VGG19_model
+from train import Train_Mask, Train_Face
+from prediction import Load_model
+import warnings
+warnings.filterwarnings('ignore')
+
+def main():
+    parser = argparse.ArgumentParser(description='Train the Mask removal network',
+                            formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--mode', default='multi_test', choices=['mask-train', 'face-train', 'single-test', 'multi-test'], dest='mode')
+    parser.add_argument('--dir_test', default='./test', dest='dir_test')
+    
+    PATH = os.getcwd()
+    train_path = PATH+'/tests'                
+    test_path = parser.parse_args().dir_test
+    mask_checkpoint_dir = PATH+'/mask32_checkpoints'
+    face_checkpoint_dir = PATH+'/face_checkpoints'
+    dis_checkpoint_dir= PATH+'/dis_checkpoints'
+    
+    if parser.parse_args().mode == 'single-test':     # Visualize one image in a folder
+        mask_G, face_G = Mask_G(filters=32), Face_G(filters=64)
+        test = Load_model(mask_G, face_G, mask_checkpoint_dir=mask_checkpoint_dir, face_checkpoint_dir=face_checkpoint_dir)
+        test.load()
+        test.one_predict(test_path)
+    elif parser.parse_args().mode == 'multi-test':    # Visualize up to 4 images in a folder
+        mask_G, face_G = Mask_G(filters=32), Face_G(filters=64)
+        test = Load_model(mask_G, face_G, mask_checkpoint_dir=mask_checkpoint_dir, face_checkpoint_dir=face_checkpoint_dir)
+        test.load()
+        test.multiple_predict(test_path)
+    else:
+        BATCH_SIZE = 8
+        trainset = Dataset(file_path=train_path, batch_size=BATCH_SIZE)
+        trainset = trainset.make_train()
+        
+        with tf.device('/gpu:0'):
+            if parser.parse_args().mode == 'mask-train':
+                mask_G= Mask_G(filters=32)
+                mask_train = Train_Mask(mask_G, checkpoint_dir=mask_checkpoint_dir)
+                mask_train.fit(trainset, epochs=5)
+            elif parser.parse_args().mode == 'face-train':       
+                mask_G, face_G, face_D_whole, face_D_region, vgg19 = Mask_G(filters=32), Face_G(filters=64), Face_D_whole(filters=64), Face_D_region(filters=64), VGG19_model()
+                face_train = Train_Face(mask_G, face_G, face_D_whole, face_D_region, vgg19,
+                                mask_checkpoint_dir=mask_checkpoint_dir, face_checkpoint_dir=face_checkpoint_dir, dis_checkpoint_dir=dis_checkpoint_dir)
+                face_train.fit(trainset, epochs=5)
+
+if __name__ == '__main__':
+    main()
+    
