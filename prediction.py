656359300ac596d491d979c46dbed2217c926d8d
wocns1457 <wocns1457@gmail.com>
2022-02-27 19:36:34 +0900

    Modified prediction.py

--- prediction.py
+++ prediction.py
@@ -1,11 +1,8 @@
 # import cv2
-import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
-from model import *
-
-import warnings
-warnings.filterwarnings('ignore')
+from utils import noise_processing
+from datasets import Dataset
 
 class Load_model():
     def __init__(self, mask_model, face_model, mask_checkpoint_dir, face_checkpoint_dir):
@@ -19,7 +16,6 @@ class Load_model():
         
         self.mask_checkpoint_dir = mask_checkpoint_dir
         self.face_checkpoint_dir = face_checkpoint_dir
-        
 
         self.mask_checkpoint = tf.train.Checkpoint(generator_optimizer=self.mask_optimizer,
                                             generator=self.mask_model)        
@@ -30,21 +26,56 @@ class Load_model():
         self.mask_checkpoint.restore(tf.train.latest_checkpoint(self.mask_checkpoint_dir))
         self.face_checkpoint.restore(tf.train.latest_checkpoint(self.face_checkpoint_dir))
     
-# def noise_processing(generate_image):
-#     generate_image = generate_image.numpy()
-#     batch, height, width  = generate_image.shape[0], generate_image.shape[1], generate_image.shape[2]
-#     generate_image = generate_image[:, :, :, 0]
-#     k = cv2.getStructuringElement(cv2.MORPH_RECT, (1,1))
-#     for i in range(batch):
-#         # generate_image = generate_image[i, :, :, :]
-#         generate_image[i]= cv2.erode(generate_image[i], k)             #  mask  Morphology 연산 전처리
-#         generate_image[i] = cv2.dilate(generate_image[i], k)
+    # local에서 1개 이미지 시각화하여 예측
+    def one_predict(self, img_dir):        
+        img = plt.imread(img_dir)
+        if img_dir.endswith('.png'):
+            img = img * 255.0
+        img = tf.keras.preprocessing.image.img_to_array(img)
+        img = tf.image.resize(img, [128, 128],
+                            method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+        img = tf.reshape(img, [1, 128, 128, 3])
+        img = tf.cast(img, tf.float32)
+        img =  (img / 127.5) - 1
+        
+        mask = self.mask_model(img, training=False)
+        mask = noise_processing(mask)
+        face = self.face_model([img, mask], training=False)
 
-#         generate_image = np.where(generate_image >= -0.9, 1, -1)
-#         generate_image = tf.convert_to_tensor(generate_image, dtype=tf.float32)
-#         generate_image = tf.reshape(generate_image, [batch, height, width , 1])
-#         return generate_image
+        plt.figure(figsize=(7,7))
+        plt.subplot(1, 2, 1)
+        plt.title('Image with mask')
+        plt.imshow(img[0] * 0.5 + 0.5)
+        plt.axis('off')
+        
+        plt.subplot(1, 2, 2)
+        plt.title('Prediction Image')
+        plt.imshow(face[0] * 0.5 + 0.5)
+        plt.axis('off')
+        plt.show()
+        
+    # local에서 여러개 이미지 시각화하여 예측
+    def multiple_predict(self, img_dir):     
+        testset = Dataset(file_path=img_dir, batch_size=1)
+        testset = testset.make_test()
+        img_num = len(testset)
+        
+        plt.figure(figsize=(10, 10))
+        plt.suptitle('Prediction Image', fontsize=20, y=0.7)
+        
+        for i, img in enumerate(testset):
+            mask = self.mask_model(img, training=False)
+            process_img = noise_processing(img, mask)
+            pred = self.face_model(process_img, training=False)
+            pred = tf.concat([img, pred], axis=1)
 
+            plt.subplot(1, img_num, i+1)
+            plt.imshow(pred[0] * 0.5 + 0.5)
+            plt.axis('off') 
+        plt.show()
+        
+        
+# Web app을 통한 예측
 def pred(img_dir, mask_model, face_model):
     img = plt.imread(img_dir)
     img_name = img_dir.split('/')[-1]
@@ -66,5 +97,3 @@ def pred(img_dir, mask_model, face_model):
     face = tf.image.resize(face, [height, width],
                     method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
     tf.keras.preprocessing.image.save_img("flask_app/static/prediction/"+img_name, face, scale=True)
-        
-
