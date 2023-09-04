bd7ef18ba4444397fcc20b5edb1f51f70c885cff
deokjin <90086668+wocns1457@users.noreply.github.com>
2023-09-04 23:15:13 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,13 +1,11 @@
-import cv2
 import os
+import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
 plt.ion()
 figure, ax = plt.subplots(figsize=(8,8))
-
-np.random.seed(42)
 
 def noise_processing(generate_image):
     """
@@ -22,71 +20,37 @@ def noise_processing(generate_image):
     generate_image = generate_image[:, :, :, 0]
     k = cv2.getStructuringElement(cv2.MORPH_RECT, (1,1))
     for i in range(batch):
-        generate_image[i]= cv2.erode(generate_image[i], k)             #  mask  Morphology 연산 전처리
+        generate_image[i]= cv2.erode(generate_image[i], k)             #  mask Morphology 연산 전처리
         generate_image[i] = cv2.dilate(generate_image[i], k)
     generate_image = np.where(generate_image >= -0.9, 1, -1)
     generate_image = tf.convert_to_tensor(generate_image, dtype=tf.float32)
     generate_image = tf.reshape(generate_image, [batch, height, width , 1])
     return generate_image
 
-def mask_training_visualization(model, test_input, tar, epoch, step):
-    """
-    mask model training visualization
-    Args:
-        model : Mask Generate model
-        test_input : input image
-        tar : ground truth image
-        epoch, step : training epoch, step
-    """
-    
-    save_dir = './mask32_training'
-    if not os.path.exists(save_dir):
-      os.mkdir(save_dir)  
-
-    prediction = model(test_input, training=False)
-    display_list = [test_input[0], tar[0], prediction[0, :, :, 0]]
-    title = ['Input Image', 'Ground Truth', 'Predicted Image']
-    
-    for i in range(3):
-        plt.subplot(1, 3, i+1)
-        plt.title(title[i])
-        plt.imshow(display_list[i])
-        plt.axis('off')
-        
-    plt.savefig(save_dir+"/epoch_{epoch}_step_{step}.png".format(epoch=epoch+1, step=step+1))
-    plt.show()
-    
-    figure.canvas.draw()
-    figure.canvas.flush_events()
-
-
-def face_training_visualization(model, test_input, binary_input, tar, epoch, step):
+def face_training_visualization(model, test_input, mask_map, tar, epoch, step, save_dir='./face32_training'):
     """
     fece model training visualization
     Args:
         model : Mask Generate model
         test_input : input image
-        binary_input : Mask Generate model out
+        mask_map : Mask Generate model out
         tar : ground truth image
         epoch, step : training epoch, step
     """
-    
-    save_dir = './face32_training'
     if not os.path.exists(save_dir):
-      os.mkdir(save_dir)  
-      
-    prediction = model([test_input, binary_input], training=False)
-    print(prediction.shape)
-    display_list = [test_input[0], tar[0], prediction[0, :, :, :]]
-    title = ['Input Image', 'Ground Truth', 'Predicted Image']
-    
-    for i in range(3):
-        plt.subplot(1, 3, i+1)
+      os.mkdir(save_dir)
+
+    prediction = model([test_input, mask_map], training=False)
+    display_list = [test_input[0], tar[0], prediction[0], mask_map[0]]
+    title = ['Input Image', 'Ground Truth', 'Predicted Image', 'Predicted mask']
+
+    for i in range(4):
+        plt.subplot(1, 4, i+1)
         plt.title(title[i])
         plt.imshow(display_list[i] * 0.5 + 0.5)
         plt.axis('off')
-    plt.savefig(save_dir+"/epoch_{epoch}_step_{step}.png".format(epoch=epoch+1, step=step+1))
+    plt.savefig(save_dir+"/epoch_{epoch}_step_{step}.png".format(epoch=epoch, step=step))
     plt.show()
-    
+
     figure.canvas.draw()
     figure.canvas.flush_events()
